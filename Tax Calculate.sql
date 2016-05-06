f6eb0d9218b028fede3951bba6badf8ad5da3fc3
wiratk <wirat_khongthai@hotmail.com>
2016-05-06 16:02:34 +0700

    Update Tax Calculate.sql

--- Tax Calculate.sql
+++ Tax Calculate.sql
@@ -1,1 +1,4 @@
-select
+select sum(case when (LEAST(tot_amt,upper)-lower+1)>0 then((LEAST(tot_amt,upper)-lower+1)*percent/100)
+else 0 end) net_total_amt from 
+(select t.*,1100000 tot_amt from tax_rate t
+)t
